3c33d294cca84d4575ceadc2dfd468d90e767951
AshTech19 <ayesha19ab@gmail.com>
2021-11-08 18:37:58 -0500

    State Management & Lifecycle Events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React,{Component} from 'react'
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
+
+/* Before adding state
 const contacts = [
   {
   "id": "karen",
@@ -20,13 +23,65 @@ const contacts = [
   "avatarURL": "http://localhost:5001/tyler.jpg"
 }
 ]
+*/
 
 class App extends Component{
+  // After adding state
+  state = {
+    /*Before adding lifecycle event 
+    contacts:[
+      {
+      "id": "karen",
+      "name": "Karen Isgrigg",
+      "handle": "karen_isgrigg",
+      "avatarURL": "http://localhost:5001/karen.jpg"
+    },
+    {
+      "id": "richard",
+      "name": "Richard Kalehoff",
+      "handle": "richardkalehoff",
+      "avatarURL": "http://localhost:5001/richard.jpg"
+    },
+    {
+      "id": "tyler",
+      "name": "Tyler McGinnis",
+      "handle": "tylermcginnis",
+      "avatarURL": "http://localhost:5001/tyler.jpg"
+    }
+    ]
+    */
+   // After adding lifecycle event
+   contacts:[]
+  }
+
+  componentDidMount(){
+    ContactsAPI.getAll()
+      .then((contacts)=>{
+        this.setState(()=>({
+          contacts
+        }))
+      })
+  }
+
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) =>{
+        return c.id !== contact.id
+      })
+    })) 
+    ContactsAPI.remove(contact)
+  }
   render(){
     return(
 
       <div>
+        {/*Before adding state
         <ListContacts contacts = {contacts}/>
+        */}
+        <ListContacts 
+          contacts = {this.state.contacts}
+          onDeleteContact = {this.removeContact}
+        />
       </div>
     )
   }
